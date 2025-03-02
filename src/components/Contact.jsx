4eaf5e11df21b075aebddaa08a2feddee463ff96
Nikhil Sai Ramini <_01nikhil@Nikhils-MacBook-Air-2.local>
2025-03-02 13:53:30 -0500

    Updated feature

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -49,9 +49,9 @@ const Contact = () => {
 				EMAIL_JS_TEMPLATE_ID,
 				{
 					from_name: username,
-					to_name: "Nikhil Sai Ramini",
+					to_name: "Nikhil",
 					reply_to: user_email,
-					to_email: "32Nikhill@gmail.com",
+					to_email: "32nikhill@gmail.com",
 					message: user_message,
 				},
 				EMAIL_JS_PUBLIC_KEY
